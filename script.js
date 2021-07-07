c7059945524864d0e3e5ed60cc0d08d90d0398bf
FrontEndDouwe <71258554+FrontEndDouwe@users.noreply.github.com>
2021-07-07 18:03:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//Maakt de gehele div met de class githubSchool clickable
+document.getElementById("githubSchool").onclick = function(){
+    location.href="https://github.com/frontenddouwe";
+};
+
+
+//Maakt de gehele div met de class githubPrive clickable
+document.getElementById("githubPrive").onclick = function(){
+    location.href="https://github.com/douwescript";
+};
